8a6fe827881e92af3b6956f3053bfec5d610db98
sarahbicknell <166096006+sarahbicknell@users.noreply.github.com>
2024-06-12 13:29:53 -0400

    logic for tictactoe game

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
+const game = {
+  board: [null, null, null, null, null, null, null, null, null]
+}
+
+function checkForWin(game) {
+
+    //array of possible win configurations
+    const possibleWinPositions = [
+      [0, 1, 2], [3, 4, 5], [6, 7, 9], [0, 3, 6], [1, 4, 7,], [2, 5, 8], [0, 4, 8], [2, 4, 6]
+    ]
+
+    // checks whether a given position is a winner and returns a boolean to represent this 
+    function isWinningPosition {
+
+      //.every 
+      return null
+    }
+
+  
+
+    //using .find to call the isWinning function to return the first possible position that is a win 
+    const winningPosition = possibleWinPositions.find(winPosition => {
+      const isWin = winPosition.every(index => {
+
+      })
+    })
+
+    // if there is a winning position, check 
+    if (winningPosition) {
+      return [outcome: 'win', winner: game.board[winningPosition[0]]
+    }
+}
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div> 
+      <h1> TEEK TAK TOE</h1>
+    </div>
     </>
   )
 }
